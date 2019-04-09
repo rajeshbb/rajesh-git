16f8ab0bb87661a6770804e1e19a39c6b88377c2
rajeshbb <48983477+rajeshbb@users.noreply.github.com>
2019-04-09 11:44:02 +0530

    Add files via upload

--- register.js
+++ register.js
@@ -24,12 +24,23 @@ app.use(function (req,res,next){
 next();
 })
 
-app.get('/register',function(req,res,next){
+app.get('/register/',function(req,res,next){
+   
     dbc.any('select * from register').then((data)=>{
         res.send(data)
         
     })
 })
+app.get('/register/:username',function(req,res,next){
+    var username=req.params.username
+    dbc.any('select *from fn_getusername($1)',username).then((data)=>{
+
+        res.send(data)
+        
+    })
+})
+
+//post//
 app.use(bodyparser.json())
 app.post('/register/',function(req,res,next){
     var fname=req.body.fname
@@ -44,6 +55,35 @@ res.send(data);
     })
 })
 
+//post1//
+app.post('/register1/',function(req,res,next){
+    var fname=req.body.fname
+    var lname=req.body.lname
+    var username=req.body.username
+    var password=req.body.password
+    var email=req.body.email
+    var phone=req.body.phone
+    var gender=req.body.gender
+    dbc.any('select fn_post($1,$2,$3,$4,$5,$6,$7)',[fname,lname,username,password,email,phone,gender]).then((data)=>{
+res.send({"message":"saved sucessfully"});
+    })
+})
+// user//
+
+app.post("/user/login", (req, res,next) => {
+    var u = req.body.username;
+    var p = req.body.password;
+    console.log(req.body);
+    dbc.any('select * from register where username = $1 and password =$2 ', [u, p]).
+    then((result) => {
+    if(result.length>0) {
+        res.send({"valid":"true"})
+    }  else{
+        res.send({"valid":"false"})
+    }
+    
+    })
+})
 app.put('/register',function(req,res,next){
 
 var fname=req.body.fname
@@ -55,13 +95,13 @@ var gender=req.body.gender
 var username=req.body.username
 dbc.any('select fn_update3($1,$2,$3,$4,$5,$6,$7)',[fname,lname,password,email,phone,gender,username]).
 then((data)=>{
-    res.send(data)
+    res.send({"message": "sucessfuly"})
 })
 })
 app.delete('/register/:username',function(req,res,next){
     var username=req.params.username
     dbc.any('select fn_delete1($1)',username).then((data)=>{
-res.send(data)
+res.send({'message':'delete sucessfully'})
     })
 })
 app.listen(8000,(err)=>{
